23e7d32c4b729de086b83aa6a5f08acaf12a74b4
aatack <alexanderjtatack@gmail.com>
2019-08-11 22:42:31 +0100

    Write to file immediately after loading

--- loadexternal.lua
+++ loadexternal.lua
@@ -1,3 +1,6 @@
+local internet = require "internet"
+local computer = require "computer"
+
 local args = {...}
 
 -- URL from which the file should be loaded; should only contain text
@@ -9,17 +12,28 @@ local destination = args[2]
 --- Load the given file and save it to the destination as a lua file.
 function loadfile(url, savelocation)
   local filetext = textfromurl(url)
-  print(filetext)
-  print(type(filetext))
+  print("Writing to " .. savelocation)
+  writefile(savelocation .. ".lua", filetext)
 end
 
 --- Send a request to the given URL and return the result as a string.
 function textfromurl(url)
   local output = ""
-  for section in internet.request(url) do
+  local response = internet.request(url)
+
+  for section in response do
     output = output .. section
   end
   return output
 end
 
+--- Write text to the given file.  The extension should be included in
+-- the file's location.
+function writefile(location, text)
+  file = io.open(location, "w")
+  file:write(text)
+  file:close()
+end
+
 loadfile(sourceurl, destination)
+computer.shutdown(true)
